a01414ba0019a988d64d0ec2285a08e6900ead58
whimOfGod <78544822+whimOfGod@users.noreply.github.com>
2023-05-20 22:38:03 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function showSidebarMenu() {
+    document.querySelector(".myDropdownMenu").style.display = "block";
+}
+
+function hideSidebarMenu() {
+    document.querySelector(".myDropdownMenu").style.display = "none";
+}
+/*const iconDropdown = document.querySelector(".myDropdownMenu");
+const iconButton = document.querySelector(".myIconButton");
+
+iconButton.addEventListener('click', () =>{
+    iconDropdown.forEach(iconDrop =>{
+        iconDrop.classList.toggle("dropItem");
+    })  
+}) */
+
+/*function showSidebarMenu() {
+    document.querySelectorAll(".myDropdownMenu").style.display = "block";   
+}
+console.log(showSidebarMenu());
+
+function hideSidebarMenu() {
+    document.querySelectorAll(".myDropdownMenu").style.display = "none";
+  }
+*/
+
+/*function showSidebarMenu() {
+    document.querySelectorAll(".myDropdownMenu").style.display = "block";   
+}
+console.log(showSidebarMenu());
+
+function hideSidebarMenu() {
+    document.querySelectorAll(".myDropdownMenu").style.display = "none";
+  }
+*/
---
+++ reconnect.php
@@ -0,0 +1,40 @@
+<?php 
+    session_start();
+// connexion à la base de donnée
+     require "template/connexion.php";
+     //préparation , exécution, récupération de tous les résultats sous forme d'un tableau associatif dans la variable
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Whisp/connexion</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/c6879e030e.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="inscriptionBody">
+
+    <?php include 'template/header.php' ?>
+        <main class="suscribMain">
+        <!--<div class="imageWhisp"><img src="images/splash.png" alt="" style="height: 700px;width: 700px;"></div>-->
+            <div class="myFormular">
+            <form method="POST" action="template/login.php">
+                <input type="email" name="mail" placeholder="Votre adresse e-mail" required>
+                <input type="password" name="password" placeholder="Mot de passe" required>
+                <button type="submit">Connexion</button>
+                <a href="inscription.php">inscrivez-vous ?</a>
+            </form>>
+            </div>
+        </main>
+ 
+    <?php include 'template/footer.php' ?>
+
+    <script src="js/main.js">
+    </script>
+</body>
+</html>
