4cc5e5350c2d7e25559c7436f149d58f3468273d
aaron kolingba <78544822+whimOfGod@users.noreply.github.com>
2023-06-19 22:57:54 +0200

    Add files via upload

--- whisp_db.sql
+++ whisp_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : dim. 21 mai 2023 à 21:56
+-- Généré le : dim. 18 juin 2023 à 21:53
 -- Version du serveur : 8.0.30
 -- Version de PHP : 8.1.10
 
@@ -41,23 +41,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`users_id`, `nom`, `pseudo`, `mail`, `my_password`, `avatar`) VALUES
-(43, 'greg', 'thegragsatalieon', 'nikomok@gmail.com', '45478879', ''),
 (44, 'aaron kolins', 'TheHybridddd', 'tillduskdawn@gmail.com', '65584984951616', ''),
 (45, 'Damon salvatore', 'TheHybrid', 'vampirediairies@gmail.com', '26849889519849', ''),
-(46, 'wellcrust', 'theWiewer', 'mickyklaus@gmail.com', '268488189+41', ''),
-(47, 'aaron kolins', 'TheHybrid', 'deathstars@gmail.com', '7852752572', ''),
-(48, 'mandalorian', 'panda', 'pandacrut@gmail.com', '151518918951', NULL),
-(49, 'sherlock holmes', 'sherky', 'sherlockholmes@gmail.com', '1658+61651', NULL),
-(50, 'mark landers', 'landers98', 'markyland@gmail.com', '21686151981603', '743387.png'),
-(51, 'jack pearl', 'pearl harbor', 'pearlharbor45@gmail.com', '1681695198851+6', NULL),
-(52, 'Damon salvatore', 'vampire98', 'thevampirediairies@gmail.com', '123', NULL),
-(53, 'hasboul', 'hasboula78', 'hasba@gmail.com', '145', 'IMG_5311 2.JPG'),
-(54, 'aaron kolins', '15', 'mickayklaus@gmail.com', '4484', '264254.jpg'),
-(55, 'johnny helmet', 'quaterion', 'quaterion41@gmail.com', '12345', NULL),
-(56, 'jack dwo', 'jackomiel', 'jackson@gmail.com', '12346', 'IMG_5311 2.JPG'),
-(57, 'hercule', 'power74', 'powerduskdawn@gmail.com', '478965', '743387.png'),
-(58, 'meugi', 'warano', 'warano12@gmail.com', '48965', '924242.jpg'),
-(59, 'livai', 'slayer77', 'attackontitan@gmail.com', '12345788', NULL);
+(59, 'livai', 'slayer77', 'attackontitan@gmail.com', '12345788', NULL),
+(62, 'joseph jotaro', 'jojo', 'joestar@gmail.com', '012345', NULL),
+(63, 'loly brook', 'brooklyn', 'brooky@gmail.com', '12345kgb', '02.jpg'),
+(64, 'mirai', 'Daizen', 'lescrotdugame@gmail.com', '0000<à', '264254.jpg'),
+(65, 'mandalorian', 'themandalorian', 'manda@gmail.com', '$2y$10$sW1Egil7obPWQS8tpIooauDXIcz2Mde7PXtZ4703gsTnrzTHJxxEy', 'IMG_5310.JPG'),
+(66, 'Michael KLAUS', 'furyroad', 'mickyklaus@gmail.com', '$2y$10$Y6C9QY9J72NKyKNXeRiPvOZjIbDD3t9eevYZ6XxbIOvObYxKGEldq', 'IMG_5324.JPG'),
+(67, 'adidas', 'yeezy', 'yeezy@gmail.com', '$2y$10$nGSEBv8BJjzrWwpluOP9wu9wyPO2/wJQ8GlnbyesD6iZq2bpWy/Lq', 'Group.png'),
+(68, 'hulk', 'hulk', 'marvel@gmail.com', '$2y$10$.eM0rrDPFYbIGRSeA9MJDuP//X5EfGPEzO9vge0QXSpOC/R4DnuBu', 'IMG_5312.JPG');
 
 -- --------------------------------------------------------
 
@@ -70,17 +63,21 @@ CREATE TABLE `whisps` (
   `user_id` int NOT NULL,
   `tweet` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `media` tinyblob
+  `media` tinyblob,
+  `tag` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `whisps`
 --
 
-INSERT INTO `whisps` (`whisps_id`, `user_id`, `tweet`, `date`, `media`) VALUES
-(149, 47, 'ça va être une journée de folie', '2023-05-20 19:53:04', NULL),
-(159, 44, 'tremblez de peur !!!!', '2023-05-20 20:45:16', 0x3633323535342e706e67),
-(178, 59, 'la saison 2 de JJK va être un banger !! attention spoil ', '2023-05-21 23:28:54', NULL);
+INSERT INTO `whisps` (`whisps_id`, `user_id`, `tweet`, `date`, `media`, `tag`) VALUES
+(187, 63, 'Unlimited Blade : hollow parade ! c\'est dope ', '2023-06-18 10:16:36', 0x3738353432352e706e67, ''),
+(188, 63, 'Mais les autres suites qu\'ils ont sortis ne sont pas si dope que ça , à part Fate zero qui est un banger de fou furieux ', '2023-06-18 10:17:56', NULL, ''),
+(190, 63, 'The hypest vilain in the world of anime ', '2023-06-18 10:35:04', 0x3937353136352e6a7067, ''),
+(191, 63, 'La saison 2 est bientôt là, vous n\'êtes pas prêt les gars, lisez les scans wesh !!!!', '2023-06-18 10:37:38', 0x313135303639342e6a7067, ''),
+(195, 67, 'One punch ', '2023-06-18 20:33:18', 0x3932343234322e6a7067, NULL),
+(196, 67, 'domain expansion !', '2023-06-18 20:49:46', 0x313330313136352e6a7067, 'red');
 
 --
 -- Index pour les tables déchargées
@@ -106,13 +103,13 @@ ALTER TABLE `whisps`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `users_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `users_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- AUTO_INCREMENT pour la table `whisps`
 --
 ALTER TABLE `whisps`
-  MODIFY `whisps_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=179;
+  MODIFY `whisps_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=199;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
